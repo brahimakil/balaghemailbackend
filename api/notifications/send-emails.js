9e0b454f092eaed13c16578384d17f77604e663f
ibrahim akil <brhimakil.1@gmail.com>
2025-09-25 00:20:46 +0300

    dded

--- api/notifications/send-emails.js
+++ api/notifications/send-emails.js
@@ -9,7 +9,7 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Max-Age', '86400');
     return res.status(200).end();
   }
-
+ 
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
