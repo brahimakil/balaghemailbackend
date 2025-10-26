c4d6294abb441e2cb89fef8d3530a327c38c6980
ibrahim akil <brhimakil.1@gmail.com>
2025-10-26 21:41:17 +0200

    added

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '50mb' })); // Increase limit for video metadata
 
 // Import the Vercel functions
 const sendEmailsHandler = require('./api/notifications/send-emails');
-const youtubeUploadHandler = require('./api/youtube/upload');
+const youtubeUploadHandler = require('./api/youtube/upload'); 
 
 // Routes for local testing
 app.post('/api/notifications/send-emails', sendEmailsHandler);
